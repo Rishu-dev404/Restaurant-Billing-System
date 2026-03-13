ecaeb34cc4d7d63c12bc92aa5bec56b1213a0af1
Rishuchakravarty55 <rishuchakravorty936@gmail.com>
2026-03-14 00:11:18 +0530

    adding a menu

--- Project.cpp
+++ Project.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
+// Menu data
+string itemNames[5] = {"Burger", "Pizza", "Pasta", "Cold Drink", "Ice Cream"};
+float itemPrices[5] = {50, 120, 90, 30, 40};
+
+// Function to display menu
+void displayMenu()
+{
+    cout << "===== RESTAURANT MENU =====" << endl;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i + 1 << ". " << itemNames[i]
+             << " - Rs." << itemPrices[i] << endl;
+    }
+    cout << "===========================" << endl;
+}
+
+int main()
+{
+    displayMenu();
     return 0;
 }
---
+++ Project_run.bat
@@ -0,0 +1,8 @@
+@echo off
+set start_time=%time%
+"e:\Galgotias University\2nd Sem\OOPS using C++\ETE Project\2nd-Sem-Project\Project.exe"
+set end_time=%time%
+echo.
+echo Nhan phim bat ki de thoat...
+pause > nul
+exit
