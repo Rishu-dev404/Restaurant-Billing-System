ab7f1cca57fa378d3f8a36068ecfb681f77f6bfd
Rishuchakravarty55 <rishuchakravorty936@gmail.com>
2026-03-18 09:26:58 +0530

    Edited

--- Project.cpp
+++ Project.cpp
@@ -8,9 +8,8 @@ float itemPrices[19] = {70, 150, 90, 30, 40, 60, 65, 65, 40, 80, 65, 40, 55, 70,
 
 // Function to display menu
 void displayMenu() {
-    cout << "==============================" << endl;
+
     cout << "       RESTAURANT MENU        " << endl;
-    cout << "==============================" << endl;
     for (int i = 0; i < 19; i++) {
         cout << i+1 << ". " << itemNames[i]
              << "\t\t Rs." << itemPrices[i] << endl;
